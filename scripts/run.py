cce7030a924d3892d74b537b69819ad9fc1ae645
tomholt1 <137874152+tomholt1@users.noreply.github.com>
2024-03-13 13:54:41 +0000

    switch to write_local_folder_to_s3

--- scripts/run.py
+++ scripts/run.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 from dataengineeringutils3.s3 import (
-    write_local_file_to_s3,
+    write_local_folder_to_s3,
 )
 
 run = os.getenv("RUN")
@@ -9,6 +9,6 @@ file_to_write = os.getenv("PATH")
 write_outpath = os.getenv("OUTPATH")
 
 if run == "write":
-    write_local_file_to_s3(file_to_write, write_outpath, overwrite=True)
+    write_local_folder_to_s3(file_to_write, write_outpath, overwrite=True)
 else:
     raise ValueError(f"Bad RUN env var. Got {run}. Expected 'write'.")
